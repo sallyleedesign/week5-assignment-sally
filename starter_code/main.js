ced0d7b55c4098f54b4a500355913cd6d4c11074
sallyleedesign <saebomlee.design@gmail.com>
2017-10-02 20:04:07 +1100

    clear background added

--- starter_code/main.js
+++ starter_code/main.js
@@ -3,16 +3,26 @@ $('#submit-btn').click(changeBackground);
 function changeBackground(event) {
   event.preventDefault();
   var userInput = $('#city-type').val();
-  if (userInput === 'nyc' || userInput === 'new york') {
+  userInput = userInput.toLowerCase();
+  if (userInput === 'nyc' || userInput === 'new york'|| userInput === 'new york city') {
+    clearBackground();
     $('body').addClass('nyc');
-  }  else if (userInput === 'sf' || userInput === 'san fransisco') {
+  } else if (userInput === 'sf' || userInput === 'san fransisco') {
+    clearBackground();
     $('body').addClass('sf');
-  } else if (userInput === 'la' || userInput === 'Los Angeles') {
+    } else if (userInput === 'la' || userInput === 'los angeles') {
+    clearBackground();
     $('body').addClass('la');
-  } else if (userInput === 'Austin' || userInput === 'ATX') {
+  } else if (userInput === 'austin' || userInput === 'atx') {
+    clearBackground();
     $('body').addClass('austin');
-  } else {
-    (userInput === 'Sydney' || userInput === 'SYD')
+  } else if (userInput === 'sydney' || userInput === 'syd') {
+    clearBackground();
     $('body').addClass('sydney');
   }
 }
+
+  function clearBackground() {
+  $('body').removeClass('nyc sf la austin sydney')
+
+  }
